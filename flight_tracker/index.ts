e8b67b01fb3b181790120e0c4d87bcc73e57deff
echutch <echutch429@gmail.com>
2025-04-04 23:07:45 -0500

    Set up for connecting to database

--- flight_tracker/index.ts
+++ flight_tracker/index.ts
@@ -1,13 +1,9 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const PORT = 3000;
+const PORT = 3007;
 
 app.use(express.json());
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to the Flight Tracker API');
-});
 
 app.get('/api/', (req: Request, res: Response) => {
     res.send('API of Flight Tracker');
